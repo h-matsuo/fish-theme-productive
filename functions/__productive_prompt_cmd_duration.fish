db48384ef639f3d0cf8f9cb9a2ad028a625da70b
Hiroyuki Matsuo <h.matsuo.engineer@gmail.com>
2019-05-02 12:27:42 +0900

    Fix: font weight of command duration

--- functions/__productive_prompt_cmd_duration.fish
+++ functions/__productive_prompt_cmd_duration.fish
@@ -5,7 +5,7 @@ function __productive_prompt_cmd_duration \
   set -l _cmd_duration (math -s 1 $argv[1] / 1000)
 
   if [ $_cmd_duration -gt $_duration_threshold_sec ]
-    set_color -o ff8700 brred
+    set_color ff8700 brred
     echo -n "Duration: $_cmd_duration s"
     set_color normal
     echo -n ' '
