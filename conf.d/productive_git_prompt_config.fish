cad38be591e7b8d531702bf9c30bce9a5a3df797
Hiroyuki Matsuo <h.matsuo.engineer@gmail.com>
2019-05-06 17:27:42 +0900

    Delete color scheme settings from conf.d

---
+++ conf.d/productive_git_prompt_config.fish
@@ -0,0 +1,8 @@
+# Configure git_prompt
+set -g __fish_git_prompt_show_informative_status 'yes'
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_showstashstate 'yes'
+set -g __fish_git_prompt_showuntrackedfiles 'yes'
+set -g __fish_git_prompt_showupstream 'yes'
+set -g __fish_git_prompt_showcolorhints 'yes'
+set -g __fish_git_prompt_color_branch brcyan
